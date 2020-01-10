6e1b8a40223aa021a68c4901f48a900582183204
Mark Muskardin <mark.muskardin@gmail.com>
2020-01-09 18:57:50 -0800

    updated text

--- src/containers/HomeView/index.js
+++ src/containers/HomeView/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class HomeView extends Component {
   render() {
     return (
-      <div className="container">Welcome to React DApp Boilerplate!</div>
+      <div className="container">Welcome to Smart Contract UX Boilerplate!</div>
     )
   }
 }
